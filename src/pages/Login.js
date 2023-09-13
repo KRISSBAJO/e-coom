e77adb7e894cb932c6eb40e3a1d68c4a97b2ce8a
LogaXP <krissbajo@logaxp.com>
2023-09-12 22:31:41 -0500

    Update guest login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,6 +40,37 @@ export const Login = () => {
       }
   }
   
+
+async function handleLoginGuest() {
+    email.current.value = process.env.REACT_APP_GUEST_ACCESS;
+    password.current.value = process.env.REACT_APP_GUEST_PIN;
+    try {
+        const authDetail = {
+            email: email.current.value,
+            password: password.current.value
+        };
+        const data = await login(authDetail);
+        if (data.success) {
+            toast.success(data.message || "Login Successful", {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+            navigate("/products");
+        } else {
+            toast.error(data.message || "Login Failed", {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+            navigate("/login");
+        }
+    } catch (err) {
+        toast.error(`Error during login: ${err.message}`, {
+            position: toast.POSITION.BOTTOM_CENTER
+        });
+    }
+}
+
+
+
+
   return (
     <main>
       <section>
@@ -56,7 +87,7 @@ export const Login = () => {
           </div>
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
         </form>
-        <button  className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
+        <button onClick={handleLoginGuest} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
     </main>
   );
 }
